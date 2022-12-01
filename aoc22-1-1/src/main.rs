a0057876cd39c216fd06872f6df845522f1c0920
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2022-12-01 21:32:35 +0100

    Day 1 Puzzle 2

--- aoc22-1-1/src/main.rs
+++ aoc22-1-1/src/main.rs
@@ -6,17 +6,20 @@ fn main() {
         fs::read_to_string(filename).expect("Something went wrong reading the file");
     let mut max_calories: i64 = 0;
     let mut cum_calories: i64 = 0;
+    let mut vec: Vec<i64> = Vec::new();
     for line in contents.lines() {
         if line.is_empty() {
+            vec.push(cum_calories);
             if cum_calories > max_calories {
                 max_calories = cum_calories;
             }
             cum_calories = 0;
-        }
-        else {
+        } else {
             cum_calories += line.parse::<i64>().unwrap();
         }
     }
-
-    println!("Result: {}", max_calories);
+    vec.sort();
+    vec.reverse();
+    println!("Result Pr 1: {}", max_calories);
+    println!("Result Pt 2: {}", vec[0] + vec[1] + vec[2]);
 }
